fa5608fa0264f33f829ad8b0d98f417ae82d7860
Nasramohammed <nasra_mohammed@hotmail.co.uk>
2022-12-13 17:29:28 +0000

    complete 4-GET-api-articles

--- db/models/models.js
+++ db/models/models.js
@@ -8,3 +8,21 @@ exports.fetchTopics = () => {
       return result.rows;
     });
 }
+
+
+exports.fetchArticles = () => {
+  return db
+    .query(
+      `SELECT articles.article_id, articles.author, articles.created_at, articles.title, articles.topic, articles.votes, 
+      COUNT(comments.article_id) AS comment_count 
+      FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC
+    ;` 
+  )
+    
+    .then((result) => {
+      return result.rows;
+    });
+};
