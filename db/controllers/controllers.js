f80aae692c3925f3798f550b9d83fb9cbd974251
Nasramohammed <nasra_mohammed@hotmail.co.uk>
2022-12-15 11:49:24 +0000

    complete 5GET-api-articles-article_id

--- db/controllers/controllers.js
+++ db/controllers/controllers.js
@@ -1,4 +1,4 @@
-const { fetchTopics } = require("../models/models.js");
+const { fetchTopics, selectArticleById } = require("../models/models.js");
 
 
 exports.GetTopics = (req, res, next) => {
@@ -6,5 +6,13 @@ exports.GetTopics = (req, res, next) => {
         .then((topics) => {
             res.status(200).send({ topics });
         })
-      .catch(next)
+        .catch(next)
 }
+
+exports.getArticleById = (req, res,next) => {
+const { article_id } = req.params;
+    selectArticleById(article_id).then((article) => {
+    res.status(200).send({ article })
+    })
+        .catch(next)
+};
